8f9d1fea951861fd98d06529e45e009ba51febca
kaganawa <manuel-hamza@hotmail.com>
2023-10-06 10:33:16 +0200

    refactor day1 part1

--- 2015/Day1.fsx
+++ 2015/Day1.fsx
@@ -1,12 +1,12 @@
 
 let part1 (input:string) : int =
-    (0, input)
-    ||> Seq.fold (fun (counter:int) (c:char) ->
-        match c with
-        | '(' -> counter + 1
-        | ')' -> counter - 1
+    input
+    |> Seq.map (function
+        | '(' -> 1
+        | ')' -> -1
         | _ -> failwith "wrong input"
     )
+    |> Seq.sum
 
 if fsi.CommandLineArgs.Length = 2 then
     let file = fsi.CommandLineArgs[1]
