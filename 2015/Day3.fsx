022af9c24e978aa879cb5529a47032870b452dfa
kaganawa <manuel-hamza@hotmail.com>
2023-10-06 14:31:43 +0200

    solve day3 part2

--- 2015/Day3.fsx
+++ 2015/Day3.fsx
@@ -1,4 +1,3 @@
-
 type Move =
     | North
     | South
@@ -39,24 +38,75 @@ let part1 (input:list<Move>) : int =
     |> Set.add {x=0;y=0}
     |> Set.count
 
-let part2 (input:list<Move>) : int =
-    0
+let deliver (input : seq<Move>) : seq<Position> =
+    ({x=0;y=0}, input)
+    ||> Seq.scan(fun current move -> 
+            current + makeMove move
+    )
 
+let nthElement (input : seq<_>) (n : int) : seq<_> =
+    input
+    |> Seq.indexed
+    |> Seq.filter(fun (idx, _) -> idx % n = 0 )
+    |> Seq.map snd
+
+let part2v5 (input : seq<_>) : int =
+    (({x = 0; y = 0}, {x = 0 ; y = 0}), input
+    |> Seq.indexed)
+    ||> Seq.scan(fun (santa, robot) (idx, move) -> 
+        if idx % 2 = 0 then
+            (santa + makeMove move, robot)
+        else 
+            (santa, robot + makeMove move)
+    )
+    |> Seq.collect(fun (santa, robot) -> [santa; robot])
+    |> Set.ofSeq 
+    |> Set.add {x=0; y=0}
+    |> Set.count
+
+let part2alt (input : list<Move>) : int =
+    let santa = 
+        nthElement input 2
+    let robot =
+        nthElement (Seq.skip 1 input) 2
+
+    Seq.append (deliver santa) (deliver robot)
+    |> Set.ofSeq
+    |> Set.add {x=0; y=0}
+    |> Set.count
+
+let part2 (input : list<Move>) : int =
+    let santa = 
+        input
+        |> List.indexed
+        |> List.filter(fun (idx, move) -> idx % 2 = 0)
+        |> List.map snd
+    let robot =
+        input
+        |> List.indexed
+        |> List.filter(fun (idx, move) -> idx % 2 = 1)
+        |> List.map snd
+
+    Seq.append (deliver santa) (deliver robot)
+    |> Set.ofSeq
+    |> Set.add {x=0; y=0}
+    |> Set.count
+    
 if fsi.CommandLineArgs.Length = 2 then
     let file = fsi.CommandLineArgs[1]
     System.IO.File.ReadAllText(file)
     |> parse
-    |> part1
+    |> part2v5
     |> printfn "%i"
 else
     [
-        ">", 2
-        "^>v<", 4
-        "^v^v^v^v^v", 2
+        "^v", 3
+        "^>v<", 3
+        "^v^v^v^v^v", 11
     ]
     |> Seq.map (fun (input, output) -> parse input, output)
     |> Seq.iter (fun (input, output) ->
-        if part1 input = output then
+        if part2v5 input = output then
             printfn "yay"
         else
             printfn "nay"
