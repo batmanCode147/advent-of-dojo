f2c6ce33d7f827bc46844497854521598e83472e
kaganawa <manuel-hamza@hotmail.com>
2023-10-06 10:53:28 +0200

    refactor day1 part2

--- 2015/Day1.fsx
+++ 2015/Day1.fsx
@@ -1,17 +1,38 @@
 
-let part1 (input:string) : int =
+type MoveInstruction =
+    | Up
+    | Down
+ 
+    static member ToInt = function
+        | Up -> 1
+        | Down -> -1
+
+let parse (input:string) : list<MoveInstruction> =
     input
     |> Seq.map (function
-        | '(' -> 1
-        | ')' -> -1
+        | '(' -> Up
+        | ')' -> Down
         | _ -> failwith "wrong input"
     )
+    |> Seq.toList
+
+let part1 (input:list<MoveInstruction>) : int =
+    input
+    |> Seq.map MoveInstruction.ToInt
     |> Seq.sum
+
+let part2 (input:list<MoveInstruction>) : int =
+    input
+    |> Seq.map MoveInstruction.ToInt
+    |> Seq.scan (+) 0
+    |> Seq.takeWhile (fun x -> 0 <= x)
+    |> Seq.length
 
 if fsi.CommandLineArgs.Length = 2 then
     let file = fsi.CommandLineArgs[1]
     System.IO.File.ReadAllText(file)
-    |> part1
+    |> parse
+    |> part2
     |> printfn "%i"
 else
     [
@@ -21,7 +42,8 @@ else
         "))(((((",3
         ")())())",-3
     ]
-    |> List.iter (fun (input, output) ->
+    |> Seq.map (fun (input, output) -> parse input, output)
+    |> Seq.iter (fun (input, output) ->
         if part1 input = output then
             printfn "yay"
         else
