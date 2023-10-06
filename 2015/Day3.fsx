0106d62f7ed71aebf48498cde7c71465673ae9ae
kaganawa <manuel-hamza@hotmail.com>
2023-10-06 14:52:04 +0200

    refactor day3 part1 & part2

--- 2015/Day3.fsx
+++ 2015/Day3.fsx
@@ -4,19 +4,17 @@ type Move =
     | East
     | West 
 
-type Position = { 
-    x : int
-    y : int
-} with
-    static member (+)(left: Position, right: Position) =
-        { x = (left.x + right.x); y = (left.y + right.y)} 
+type Position =
+    { x : int
+      y : int }
+    static member Zero = { x = 0; y = 0 }
 
-let makeMove(move: Move) : Position =
-   match move with
-    | North -> { x = 0; y = 1}
-    | South -> { x = 0; y = -1}
-    | East -> { x = 1; y = 0}
-    | West  -> { x = -1; y = 0}
+let updatePosition (current : Position) (delta : Move) : Position =
+    match delta with
+    | North -> { current with y = current.y + 1 }
+    | South -> { current with y = current.y - 1 }
+    | East ->  { current with x = current.x + 1 }
+    | West  -> { current with x = current.x - 1 }
 
 let parse (input:string) : list<Move> =
     input
@@ -30,73 +28,27 @@ let parse (input:string) : list<Move> =
     |> Seq.toList
 
 let part1 (input:list<Move>) : int =
-    ({x=0;y=0}, input)
-    ||> Seq.scan(fun current move -> 
-            current + makeMove move
-        ) 
+    (Position.Zero, input)
+    ||> Seq.scan updatePosition
     |> Set.ofSeq
-    |> Set.add {x=0;y=0}
+    |> Set.add Position.Zero
     |> Set.count
 
-let deliver (input : seq<Move>) : seq<Position> =
-    ({x=0;y=0}, input)
-    ||> Seq.scan(fun current move -> 
-            current + makeMove move
+let part2 (input : seq<_>) : int =
+    ((Position.Zero, Position.Zero), input)
+    ||> Seq.scan (fun (first, second) delta ->
+        (second, updatePosition first delta)
     )
-
-let nthElement (input : seq<_>) (n : int) : seq<_> =
-    input
-    |> Seq.indexed
-    |> Seq.filter(fun (idx, _) -> idx % n = 0 )
     |> Seq.map snd
-
-let part2v5 (input : seq<_>) : int =
-    (({x = 0; y = 0}, {x = 0 ; y = 0}), input
-    |> Seq.indexed)
-    ||> Seq.scan(fun (santa, robot) (idx, move) -> 
-        if idx % 2 = 0 then
-            (santa + makeMove move, robot)
-        else 
-            (santa, robot + makeMove move)
-    )
-    |> Seq.collect(fun (santa, robot) -> [santa; robot])
     |> Set.ofSeq 
-    |> Set.add {x=0; y=0}
+    |> Set.add Position.Zero
     |> Set.count
 
-let part2alt (input : list<Move>) : int =
-    let santa = 
-        nthElement input 2
-    let robot =
-        nthElement (Seq.skip 1 input) 2
-
-    Seq.append (deliver santa) (deliver robot)
-    |> Set.ofSeq
-    |> Set.add {x=0; y=0}
-    |> Set.count
-
-let part2 (input : list<Move>) : int =
-    let santa = 
-        input
-        |> List.indexed
-        |> List.filter(fun (idx, move) -> idx % 2 = 0)
-        |> List.map snd
-    let robot =
-        input
-        |> List.indexed
-        |> List.filter(fun (idx, move) -> idx % 2 = 1)
-        |> List.map snd
-
-    Seq.append (deliver santa) (deliver robot)
-    |> Set.ofSeq
-    |> Set.add {x=0; y=0}
-    |> Set.count
-    
 if fsi.CommandLineArgs.Length = 2 then
     let file = fsi.CommandLineArgs[1]
     System.IO.File.ReadAllText(file)
     |> parse
-    |> part2v5
+    |> part2
     |> printfn "%i"
 else
     [
@@ -106,7 +58,7 @@ else
     ]
     |> Seq.map (fun (input, output) -> parse input, output)
     |> Seq.iter (fun (input, output) ->
-        if part2v5 input = output then
+        if part2 input = output then
             printfn "yay"
         else
             printfn "nay"
